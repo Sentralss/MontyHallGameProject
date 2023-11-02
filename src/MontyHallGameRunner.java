4a45970a9e61f1dc30e6cf292f383eac95c5d231
BT_3W2_25 <bryantv2789@nycstudents.net>
2023-11-02 11:51:34 -0400

    initial commit

---
+++ src/MontyHallGameRunner.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class MontyHallGameRunner
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Monty Hall Game!\n");
+
+        System.out.println("There are two goats and a car randomly placed behind doors 1, 2, and 3");
+        System.out.print("Where do you think the car is? Enter 1, 2, or 3: ");
+        int door = scan.nextInt();
+
+
+
+
+
+
+
+
+
+    }
+}
